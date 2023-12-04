28749d095559739471de8911da2566ee2b0d4c45
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2023-12-04 08:38:56 -0500

    day 4

--- Aoc2023/Day04.lean
+++ Aoc2023/Day04.lean
@@ -13,10 +13,10 @@ PART 1:
 -/
 
 
-def parseCard : Parsec ((List Nat) × (List Nat)) := do
+def parseCard : Parsec (Nat × ((List Nat) × (List Nat))) := do
   let _ ← pstring "Card"
   ws
-  let _ ← natNum
+  let n ← natNum
   let _ ← pchar ':'
   ws
   let lst1 ← sepBy natNum whites
@@ -24,19 +24,19 @@ def parseCard : Parsec ((List Nat) × (List Nat)) := do
   let _ ← pchar '|'
   ws
   let lst2 ← sepBy natNum whites
-  return ⟨lst1, lst2⟩
+  return ⟨n, lst1, lst2⟩
 
 def score (n : Nat) := if n = 0 then 0 else 2^(n-1)
 
 def firstPart (input : FilePath) : IO Nat := do
   let rawdata := (← IO.FS.lines input)
   let parsed := rawdata.map fun s => s.yoloParse parseCard
-  let intersections := parsed.map fun card => card.1.filter (card.2.contains ·)
+  let intersections := parsed.map fun card => card.2.1.filter (card.2.2.contains ·)
   let scores := intersections.map fun lst => score lst.length
   return scores.sum
 
---#eval firstPart testinput
---#eval firstPart realinput
+--#eval firstPart testinput    --(ans: 13)
+--#eval firstPart realinput    --(ans: 28538)
 
 /-
 PART 2:
@@ -44,9 +44,17 @@ PART 2:
 
 def secondPart (input : FilePath) : IO Nat := do
   let rawdata := (← IO.FS.lines input)
-  return 0
+  let parsed := rawdata.map fun s => s.yoloParse parseCard
+  let intersections := parsed.map fun card => card.2.1.filter (card.2.2.contains ·)
+  let numInter := intersections.map (·.length)
+  let mut dups := mkArray numInter.size 1
+  for i in [0:numInter.size] do
+    for j in [i+1:i+1+numInter[i]!] do
+      if j < numInter.size then
+        dups := dups.set! j (dups[j]! + dups[i]!)
+  return dups.sum
 
---#eval secondPart testinput
---#eval secondPart realinput
+--#eval secondPart testinput   --(ans: 30)
+--#eval secondPart realinput   --(ans: 9425061)
 
 end Day04
