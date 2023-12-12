759fcc8ba5951447d6e4e942817117fe190db4ec
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2023-12-12 18:33:15 -0500

    day 12: add problem statement

--- Aoc2023/Day12.lean
+++ Aoc2023/Day12.lean
@@ -2,6 +2,44 @@ import Aoc2023.Utils
 
 open System Lean Lean.Parsec
 
+/- # Advent of Code 2023 Day 12
+
+## Problem statement
+
+In the giant field just outside, the springs are arranged into rows. For each row, the condition
+records show every spring and whether it is operational (.) or damaged (#). This is the part of the
+condition records that is itself damaged; for some springs, it is simply unknown (?) whether the
+spring is operational or damaged.
+
+However, the engineer that produced the condition records also duplicated some of this information
+in a different format! After the list of springs for a given row, the size of each contiguous group
+of damaged springs is listed in the order those groups appear in the row. This list always accounts
+for every damaged spring, and each number is the entire size of its contiguous group (that is,
+groups are always separated by at least one operational spring: `####` would always be 4, never 2,2).
+
+So, condition records with no unknown spring conditions might look like this:
+
+#.#.### 1,1,3
+.#...#....###. 1,1,3
+.#.###.#.###### 1,3,1,6
+####.#...#... 4,1,1
+#....######..#####. 1,6,5
+.###.##....# 3,2,1
+
+However, the condition records are partially damaged; some of the springs' conditions are actually
+unknown (?). For example:
+
+???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+
+Equipped with this information, it is your job to figure out how many different arrangements of
+operational and broken springs fit the given criteria in each row.
+-/
+
 namespace Day12
 
 def testinput1 : FilePath := "/home/fred/lean/aoc2023/input_12_test1"
